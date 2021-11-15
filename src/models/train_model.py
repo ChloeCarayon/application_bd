18ec7b110f7ec65cf46888614816d58c7c20eb9c
Chloe CARAYON <chloe.carayon@efrei.net>
2021-11-15 12:04:56 +0100

    docs: docstrings (train)

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,6 +5,7 @@ from h2o.estimators.gbm import H2OGradientBoostingEstimator
 from h2o.estimators.random_forest import H2ORandomForestEstimator
 from h2o.estimators.xgboost import H2OXGBoostEstimator
 from sklearn.utils import resample
+from h2o.automl import H2OAutoML
 
 from src.models.model_parameters import MODELS_PARAMS
 
@@ -34,40 +35,90 @@ def do_sampling(df: pd.DataFrame, label: str, downsample_coef: float = 0.75):
 
 
 def define_h2o_dataframe(df: pd.DataFrame, label: str):
+    """This function transforms a pandas DataFrame
+    into H2O DataFrame.
+    It also defines the target column.
+
+    :param df: dataset
+    :type df: pd.DataFrame
+    :param label: target
+    :type label: str
+    """
     df_h2o = h2o.H2OFrame(df)
     df_h2o[label].asfactor().levels()
     df_h2o[label] = df_h2o[label].asfactor()
     return df_h2o
 
 
-def define_features(df: pd.DataFrame, label: str):
-    features = list(df.columns)
+def define_features(df: h2o.frame.H2OFrame, label: str):
+    """This function returns the feature columns
+    in a list without the label column.
+
+    :param df: dataset
+    :type df: h2o.frame.H2OFrame
+    :param label: target
+    :type label: str
+    """
+    features = df.col_names
     features.remove(label)
     return features
+
+
 
 
 def split_dataset(
     df: h2o.frame.H2OFrame, ratio_train_test: float, ratio_validation: float
 ):
+    """This function splits dataset
+    in train, test, validation.
+
+    :param df: dataset to downsample
+    :type df: h2o.frame.H2OFrame
+    :param ratio_train_test: ratio for train/test
+    :type ratio_train_test: float
+    :param ratio_validation: ratio for validation
+    :type ratio_validation: float
+    """
     train, valid, test = df.split_frame(ratios=[ratio_train_test, ratio_validation])
     return train, valid, test
 
 
 def select_model(model_type: str):
+    """This function selects and return the
+    corresponding model.
+
+    :param model_type: model type
+    :type model_type: str
+    """
     if model_type == "xgb":
         return H2OXGBoostEstimator(**MODELS_PARAMS["Xgboost"])
     elif model_type == "gbm":
         return H2OGradientBoostingEstimator(**MODELS_PARAMS["GradientBoosting"])
+    elif model_type == "automl":
+        return H2OAutoML(max_models =25, balance_classes=True, seed =1)
     else:
         return H2ORandomForestEstimator(**MODELS_PARAMS["RandomForest"])
 
 
 def do_training(model_type: str, label: str, dataset_path: str):
+    """This function:
+    - gets the model type
+    - gets the dataset and transform it in h2o format
+    - trains model
+    - return model
+
+    :param model_type: model type
+    :type model_type: str
+    :param label: target column's name
+    :type label: str
+    :param dataset_path: path to dataset
+    :type dataset_path: str
+    """
     model = select_model(model_type)
 
     df = pd.read_csv(dataset_path)
 
-    df_h2o = define_h2o_dataframe(df)
+    df_h2o = define_h2o_dataframe(df, label)
     features = define_features(df, label)
 
     train, valid, test = split_dataset(df_h2o, 0.8, 0.1)
@@ -86,5 +137,7 @@ def train_model(model_type):
     label = "TARGET"
     dataset_path = "../data/processed/train.csv"
     model = do_training(model_type, label, dataset_path)
+    if model_type == "automl":
+        model = model.get_best_model()
     model_path = f"../models/{model_type}.zip"
     model.download_mojo(model_path)
