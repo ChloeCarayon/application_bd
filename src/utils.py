5887c94663be5bed8c537328fa52441919d9ba8e
spatzcecile <cecile.spatz@efrei.net>
2021-11-17 16:44:27 +0100

    refactor: (utils.py) add unzip functions

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,19 @@ def load_pickle(path: str):
         object = pickle.load(handle)
     return object
 
+import zipfile
+
+def unzip_file(old_path: str, new_path: str):
+    """This function unzips a file.
+
+    :param old_path: old path (for zip)
+    :type old_path: str
+    :param new_path: new path
+    :type new_path: str
+    """
+    with zipfile.ZipFile(f"{old_path}.zip", "r") as zipref:
+        zipref.extractall(new_path)
+
 
 
 
