8f93d5412dcde99f3613ac1fc1f5612fa99fe0f1
Chloe CARAYON <chloe.carayon@efrei.net>
2021-11-20 13:58:46 +0100

    feat: (tracker) add xgboost mlflow models tracker

---
+++ src/tracker.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import h2o
+import sys
+from sklearn.model_selection import train_test_split
+from h2o.estimators.xgboost import H2OXGBoostEstimator
+
+import mlflow
+from models.train_model import define_h2o_dataframe, define_features, do_sampling
+from data.make_dataset import generate_raw
+from features.build_features import generate_features
+from utils import directory_path
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+h2o.init()
+
+
+def get_preprocessed_h2o_dataset():
+    try:
+        df_path = f"{directory_path}/data/processed/application_train.csv"
+        df = pd.read_csv(df_path)
+    except Exception:
+        generate_raw("application_train.csv")
+        generate_features(directory_path, 'TARGET')
+        df_path = f"{directory_path}/data/processed/application_train.csv"
+        df = pd.read_csv(df_path)
+
+    label = 'TARGET'
+    df = do_sampling(df, label)
+    df_h2o_train, df_h2o_valid = define_train_test_validation(df, label)
+    features = define_features(df_h2o_train, label)
+    return df_h2o_train, df_h2o_valid, features, label
+
+
+def define_train_test_validation(df: pd.DataFrame, label: str):
+    df_train, df_valid = train_test_split(df, stratify = df[label], test_size=0.2)
+    df_h2o_train = define_h2o_dataframe(df_train, label)
+    df_h2o_valid = define_h2o_dataframe(df_valid, label)
+    return df_h2o_train, df_h2o_valid
+
+
+def train_xgboost_model(df_h2o_train: h2o.H2OFrame, df_h2o_validation: h2o.H2OFrame,
+                        features, label: str, ntrees:int = 200, max_depth:int = 10,
+                        learn_rate:float = 0.01, min_rows:int= 5 ):
+    with mlflow.start_run():
+        xgboost = H2OXGBoostEstimator(ntrees= ntrees, max_depth=max_depth,
+                                      learn_rate=learn_rate, min_rows=min_rows,
+                                      sample_rate= 0.9, col_sample_rate_per_tree=0.9,
+                                      score_tree_interval= 10
+                                      )
+
+
+        xgboost.train(features, label, training_frame=df_h2o_train, validation_frame=df_h2o_validation)
+
+        mlflow.log_params({"ntrees": ntrees,
+                           "max_depth": max_depth,
+                           "learn_rate": learn_rate,
+                           "min_rows": min_rows
+                           })
+
+        mlflow.log_metrics(eval_metrics(xgboost))
+
+        # log model
+        mlflow.h2o.log_model(xgboost, "model")
+
+def eval_metrics(xgboost):
+    f1 = xgboost.F1()[0][1]
+    f05 = xgboost.F0point5()[0][1]
+    f2 = xgboost.F2()[0][1]
+    auc = xgboost.auc()
+    mcc = xgboost.mcc()[0][1]
+    aucpr = xgboost.aucpr()
+
+    metrics = {
+        "f1-score": f1,
+        "f0.5-score": f05,
+        "f2-score": f2,
+        "auc": auc,
+        "mcc": mcc,
+        "aucpr": aucpr,
+    }
+    return metrics
+
+def train_mlflow(ntrees: int = 200, max_depth:int = 10,
+                 learn_rate:float = 0.01, min_rows:int = 5):
+    df_h2o_train, df_h2o_valid, features, label = get_preprocessed_h2o_dataset()
+    train_xgboost_model(df_h2o_train, df_h2o_valid, features,label,
+                        ntrees, max_depth,
+                        learn_rate, min_rows)
+
+if __name__ == "__main__":
+    ntrees = int(sys.argv[1]) if len(sys.argv) > 1 else 200
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 10
+    learn_rate = float(sys.argv[3]) if len(sys.argv) > 3 else 0.01
+    min_rows = int(sys.argv[4]) if len(sys.argv) > 4 else 5
+
+    train_mlflow(ntrees, max_depth,
+                 learn_rate, min_rows)
