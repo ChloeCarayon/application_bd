6ebd520a72b9d3cf33afb83c2c476f609a4e649a
spatzcecile <cecile.spatz@efrei.net>
2021-11-21 20:57:45 +0100

    fix: (utils.py) add docstrings

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,23 @@ directory_path = os.path.realpath(__file__).replace("src/utils.py", "")
 
 
 def dump_pickle(path: str, object):
+    """This function dump pickle
+
+    :param path: path
+    :type path: str
+    """
     with open(f"{path}.pickle", 'wb') as handle:
         pickle.dump(object, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def load_pickle(path: str):
+    """This function load pickle
+
+    :param path: path
+    :type path: str
+    :return:
+    :rtype: object
+    """
     with open(f"{path}.pickle", 'rb') as handle:
         object = pickle.load(handle)
     return object
