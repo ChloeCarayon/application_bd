546120749a0a63dd8fd642a46cb89b72c70a84ab
Chloe CARAYON <chloe.carayon@efrei.net>
2021-11-21 20:38:38 +0100

    fix: (metadata/model_parameters): modified model_parameters, add xgboost classifier

--- src/models/model_parameters.py
+++ src/models/model_parameters.py
@@ -1,11 +1,13 @@
 MODELS_PARAMS = {
     "Xgboost": {
-        "ntrees": 200,
-        "max_depth": 10,
-        "learn_rate": 0.01,
+        "ntrees": 150,
+        "max_depth": 5,
+        "learn_rate": 0.1,
         "sample_rate": 0.9,
         "col_sample_rate_per_tree": 0.9,
         "min_rows": 5,
+        "stopping_rounds": 5,
+        "stopping_metric": "AUC",
         "seed": 1,
         "score_tree_interval": 10,
     },
@@ -15,8 +17,7 @@ MODELS_PARAMS = {
         "seed": 1,
         "max_depth": 9,
         "stopping_rounds": 5,
-        "stopping_metric": "AUC",
-        "balance_classes": True,
+        "stopping_metric": "auc",
     },
     "RandomForest": {
         "nfolds": 5,
@@ -25,6 +26,14 @@ MODELS_PARAMS = {
         "max_depth": 9,
         "stopping_rounds": 5,
         "stopping_metric": "AUC",
-        "balance_classes": True,
+    },
+    "XgboostClassifier": {
+        "n_estimators": 150,
+        "max_depth": 5,
+        "min_child_weight": 1,
+        "learning_rate": 0.1,
+        "objective":'binary:logistic',
+        "colsample_bytree": 0.8,
+        "min_child_weight": 1
     },
 }
